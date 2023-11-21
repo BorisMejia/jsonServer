4639dda08e3352cebc68dce07dd9c3fe6ae04197
Boris Mejia <borisfernandez916@gmail.com>
2023-11-21 00:15:31 -0600

    Se trabaja con react dom

---
+++ front/src/components/movies/ListarPeliculas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../helpers/Header'
+
+const ListarPeliculas = () => {
+  return (
+    <div>
+        <Header/>
+        Listado
+
+    </div>
+  )
+}
+
+export default ListarPeliculas
