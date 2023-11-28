eccf7d7794dab8fadd5cdf90a4c05b0a169d7381
Boris Mejia <borisfernandez916@gmail.com>
2023-11-28 00:05:37 -0600

    Se tranaja useEffect

--- front/src/components/movies/ListarPeliculas.jsx
+++ front/src/components/movies/ListarPeliculas.jsx
@@ -2,24 +2,33 @@
 import React from 'react'
 import Header from '../helpers/Header'
 import axios from 'axios'
-import{useState} from 'react'
-import CrearPeliculas from './CrearPeliculas'
+import{useState, useEffect} from 'react'
+
+/*const getPeliculas = async () =>{
+  let respuesta = await axios.get("http://localhost:3001/peliculas")
+  console.log(respuesta.data)
+}*/
 
 const ListarPeliculas = () => {
   const [peliculas, setPeliculas] = useState([])
   const traerPeliculas = async () => {
     let listadoPeliculas = await axios.get('http://localhost:3001/peliculas')
     setPeliculas(listadoPeliculas.data)
+    console.log(peliculas)
   }
 
+  useEffect(() => {
+    traerPeliculas()
+  },[] )
   return (
-    <div>
+    <div className='peliculas'>
         <Header/>
         <h1>Listado Peliculas</h1>
-        <button onClick={traerPeliculas} type='button'>Mostrar Peliculas</button>
+        {/* <button onClick={traerPeliculas} type='button'>Mostrar Peliculas</button> */}
+        <section className='cards'>
         {
           peliculas.map((pelicula)=>(
-            <section>
+            <section className='card' key={pelicula.id}>
               <p>Nombre: {pelicula.nombre}</p>
               <p>Calificacion: {pelicula.calificacion}</p>
               <p>Clasificacion: {pelicula.clasificacion}</p>
@@ -28,6 +37,8 @@ const ListarPeliculas = () => {
             </section>
           ))
         }
+        </section>
+       
 
     </div>
   )
